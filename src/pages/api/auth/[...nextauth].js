89b1ea890fa16e91896870179025adee1b83998c
KhaLe169 <lekha1998zz@gmail.com>
2023-02-06 14:14:21 +0700

    new update

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import FacebookProvider from "next-auth/providers/facebook";
+import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
   
   providers: [
@@ -7,7 +8,10 @@ export const authOptions = {
       clientId: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     }),
-    
+      GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+  })
   ],
 }
 export default NextAuth(authOptions)
